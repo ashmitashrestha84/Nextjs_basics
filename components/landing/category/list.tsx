309c8d5a9e8f034412e41c6cc2fa54585d289bfb
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-08-12 10:56:04 +0545

    add:different states in category

--- components/landing/category/list.tsx
+++ components/landing/category/list.tsx
@@ -5,11 +5,21 @@ import { getAllCategories } from "@/api/category.api";
 import { ICategories } from "@/types/categories.types";
 
 const CategoryList = () => {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, isSuccess, isError, error } = useQuery({
     queryFn: getAllCategories,
     queryKey: ["get-all-category"],
   });
   console.log(isLoading, data);
+  if (isLoading) {
+    console.log("is Loading....");
+  }
+  if (isSuccess) {
+    console.log("sucess:", data);
+  }
+
+  if (isError) {
+    console.log("Error:", error);
+  }
   return (
     <div className="grid grid-cols-3 gap-2 mt-5">
       {data?.data?.map((category: ICategories) => (
