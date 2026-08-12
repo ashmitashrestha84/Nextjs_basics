5e50f761164b4318feaf61d016b726cf482d0181
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-08-12 10:55:01 +0545

    add:different states in product

--- components/landing/product/list.tsx
+++ components/landing/product/list.tsx
@@ -1,23 +1,33 @@
-"use client"
-import { getAllProducts } from "@/api/product.api"
-import { IProducts } from "@/types/products.types"
-import {  useQuery } from "@tanstack/react-query"
-import ProductCard from "./card"
-
+"use client";
+import { getAllProducts } from "@/api/product.api";
+import { IProducts } from "@/types/products.types";
+import { useQuery } from "@tanstack/react-query";
+import ProductCard from "./card";
 
 const ProductList = () => {
-    const {isLoading, data}=useQuery({
-        queryFn:getAllProducts,
-        queryKey:["get-all-product"],
-    })
-    console.log(data,isLoading)
+  const { isLoading, data, isSuccess, isError, error } = useQuery({
+    queryFn: getAllProducts,
+    queryKey: ["get-all-product"],
+  });
+  console.log(data, isLoading);
+  if (isLoading) {
+    console.log("is Loading....");
+  }
+  if (isSuccess) {
+    console.log("sucess:", data);
+  }
+
+  if (isError) {
+    console.log("Error:", error);
+  }
+
   return (
     <div className="grid grid-cols-3 gap-2 mt-5">
-        {data?.data?.map((product:IProducts)=>(
-            <ProductCard key={product._id} product={product} />
-        ))}
+      {data?.data?.map((product: IProducts) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
